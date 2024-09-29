4b97d077aa45a60349847e4c3131ba820758c261
Toqa-25 <toqa35abdelsalam757@gmail.com>
2024-09-29 03:43:35 +0300

    some style edit

---
+++ .history/src/components/Nav/Nav_20240929005506.js
@@ -0,0 +1,61 @@
+import {React , useState , useEffect} from 'react'
+import { Link , NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './../Main/Main.css'
+import './Nav.css'
+import Contact from '../Contact/Contact'
+import Folder from './../../images/sections/folder.svg'
+import person from './../../images/sections/lady.svg'
+
+// <FontAwesomeIcon icon="fa-solid fa-folder-open" className="icon"/>
+const Nav = () => {
+    const [showHeader , setShowHeader] = useState(false);
+    const showHeaderFun = () => {
+        setShowHeader(!showHeader)
+    }   
+    const hideHeaderFun = () => {
+        setShowHeader(false)
+        console.log("hide")
+    }  
+   
+   
+  return (
+        <div className="nav">   
+                <div className="flex-column  bar" onClick={showHeaderFun}>
+                    <FontAwesomeIcon icon="fa-solid fa-xmark " className={showHeader && " show-xmark"} />   
+                </div>  
+                <div className={"header flex-column " + (showHeader && " show-header")} >                      
+                    <div className="logo" onClick={hideHeaderFun}>
+                        <Link to="/"><span className="special-font special-color">t</span>oqa</Link>               
+                    </div>
+                    <ul className={"header-list flex-column" }>
+                        <li className="header-list-item special-color" onClick={hideHeaderFun}>
+                            <NavLink to="/projects" className="special-color"> 
+                                <img src={Folder} alt="" />                             
+                                 <span className="sec-name"> Projects </span> 
+                                 </NavLink>
+                                 </li>
+                                 <li className="header-list-item " onClick={hideHeaderFun}>
+                            <NavLink to="/about" > 
+                                 <img src={person} alt="" />                             
+                                <FontAwesomeIcon icon="fa-solid fa-user " className="icon" /> 
+                                <span className="sec-name"> about me </span>  
+                            </NavLink>
+                        </li> 
+                        <li className="header-list-item " onClick={hideHeaderFun}>
+                            <NavLink to="/services"> 
+                                <FontAwesomeIcon icon="fa-solid fa-code" className="icon" />
+                                <span className="sec-name"> services </span> 
+                            </NavLink>
+                        </li>           
+                    </ul>
+                    <Contact />
+                    </div>
+                    <button className="btn contact-btn capitalize" onClick={showHeaderFun}> contact me </button>
+        </div>
+  )
+}
+export default Nav
+
+
+
---
+++ .history/src/components/Review/Reviews_20240928202055.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReviewsData from './reviews.json'
+import './Reviews.css'
+
+const Reviews = () => {
+  return (
+    <div className="clients-reviews">
+    <h1 class="reviews-header special-color capitalize"> clients reviews </h1>
+    {ReviewsData.map((item , index) => {
+      return (
+        <div className="review-img" key={index} >
+          <img src={ require(`./../../images/reviews/${item.reviewImg}`)} alt="" className="" />                            
+        </div>
+          )
+        })}
+  </div>
+  )
+}
+
+export default Reviews
