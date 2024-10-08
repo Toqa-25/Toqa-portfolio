5850e6e92c5599be0d257b7e1d795c4cf747cd0c
Toqa-25 <toqa35abdelsalam757@gmail.com>
2024-10-08 23:01:08 +0300

    editing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
-import SkillsShow from './components/SkillsShow/SkillsShow';
+import SkillsShow from './components/SkillsShow/SkillsShow'
 
 
 
